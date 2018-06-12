9d4d4d1815481183447be6d8e1c3abf419620cbb
anhauser <anastasia.hauser@hotmail.com>
2018-06-12 15:36:20 +0200

    implemented players.length

--- src/main/scala/de/htwg/se/menschaergerdichnicht/controller/controllerComponent/controllerBaseImpl/ConCommands.scala
+++ src/main/scala/de/htwg/se/menschaergerdichnicht/controller/controllerComponent/controllerBaseImpl/ConCommands.scala
@@ -1,13 +1,13 @@
 package de.htwg.se.menschaergerdichnicht.controller.controllerComponent.controllerBaseImpl
 
-import com.google.inject.{ Guice, Inject }
+import com.google.inject.{Guice, Inject}
 import de.htwg.se.menschaergerdichnicht.model.fieldComponent.fieldBaseImpl.Dice
 import de.htwg.se.menschaergerdichnicht.util.Command
 import de.htwg.se.menschaergerdichnicht.controller.controllerComponent.GameState._
 import de.htwg.se.menschaergerdichnicht.controller.controllerComponent.PlayersChanged
-import de.htwg.se.menschaergerdichnicht.model.playerComponent.playerBaseImpl.Player
+import de.htwg.se.menschaergerdichnicht.model.playerComponent.playerBaseImpl.{Player, Players}
 
-import scala.util.{ Success, Try }
+import scala.util.{Success, Try}
 
 /**
  * Created by Anastasia on 06.06.17.
@@ -17,31 +17,31 @@ case class AddPlayer(name: String, c: Controller) extends Command {
   override def action(): Try[_] = {
     val player = Player(name, 0)
     if (c.gameState == NONE || c.gameState == PREPARE) {
-      if (c.players.getAllPlayer.length < 4) {
-        c.players = c.players.addPlayer(player)
-        if (c.players.getAllPlayer.length != player.playerId) {
-          println(c.players.getAllPlayer.length)
-          println(player.playerId - 1)
-          player.playerId = c.players.getAllPlayer.length
-          val tokens = player.getTokens()
+      if (c.players.length < 4) {
+          c.players = c.players.addPlayer(player)
+          if (c.players.length != player.playerId) {
+            println("Players length: " + c.players.length)
+            println("PlayerId - 1: ", player.playerId - 1)
+            player.playerId = c.players.length
+            val tokens = player.getTokens()
 
-          for (i <- 0 until tokens.size) {
-            if (player.playerId == 1) {
-              tokens(i).tokenId = i + 1
-              tokens(i).color = "red"
-            } else if (player.playerId == 2) {
-              tokens(i).tokenId = i + 5
-              tokens(i).color = "blue"
-            } else if (player.playerId == 3) {
-              tokens(i).tokenId = i + 9
-              tokens(i).color = "green"
-            } else {
-              tokens(i).tokenId = i + 13
-              tokens(i).color = "yellow"
+            for (i <- 0 until tokens.size) {
+              if (player.playerId == 1) {
+                tokens(i).tokenId = i + 1
+                tokens(i).color = "red"
+              } else if (player.playerId == 2) {
+                tokens(i).tokenId = i + 5
+                tokens(i).color = "blue"
+              } else if (player.playerId == 3) {
+                tokens(i).tokenId = i + 9
+                tokens(i).color = "green"
+              } else {
+                tokens(i).tokenId = i + 13
+                tokens(i).color = "yellow"
+              }
             }
-          }
 
-        }
+          }
         println("Spieler " + name + " wurde hinzugefuegt")
         c.gameState = PREPARE
         c.tui.update
@@ -87,7 +87,7 @@ case class ChooseToken(tokenId: Int, c: Controller) extends Command {
   val dice = Dice()
 
   override def action(): Try[_] = {
-    println(c.players.getCurrentPlayer)
+    println("Current Player: " + c.players.getCurrentPlayer)
     println(t)
     t match{
       case Some(token) => {
@@ -136,7 +136,7 @@ case class Play(c: Controller) extends Command {
         if (num == 0) {
           player.setDiced(num)
           c.players = c.players.nextPlayer()
-          println(c.players.getCurrentPlayer)
+          println("Current Player: " + c.players.getCurrentPlayer)
           c.gameState == SELECT
         } else {
           if (player.house.isFull(player)) {
